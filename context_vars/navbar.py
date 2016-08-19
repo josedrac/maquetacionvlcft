39d00504a63539e824bf4bff61437d8655280a57
Jose Drac <jose.drac@sopra.com>
2016-08-19 19:12:17 +0200

    new menu and logo

--- context_vars/navbar.py
+++ context_vars/navbar.py
@@ -2,57 +2,15 @@ def get_navbar():
     return {
         "main_navigation" : [
             {
-                "name"      : "Tapas Tour",
-                "id"        : "tapasTour",
+                "name"      : "Food Tours",
+                "id"        : "Food Tours",
                 "link"      : "known.html",
                 "rel"       : "Go to...",
                 "submenu"   : [
                     {
-                        "name" : "Conoce Mercadona",
+                        "name" : "Tapas Tour",
                         "link" : "known.html",
                         "rel"  : "Go to..."
-                    },
-                    {
-                        "name" : "Historia",
-                        "link" : "#",
-                        "rel"  : "Go to...",
-                        "id"   : "history"
-                    },
-                    {
-                        "name" : "Comité",
-                        "link" : "#",
-                        "rel"  : "Go to...",
-                        "id"   : "committee"
-                    },
-                    {
-                        "name" : "Responsabilidad",
-                        "link" : "#",
-                        "rel"  : "Go to...",
-                        "id"   : "responsibility"
-                    },
-                    {
-                        "name" : "Información económica",
-                        "link" : "#",
-                        "rel"  : "Go to...",
-                        "id"   : "economic"
-                    },
-                    {
-                        "name" : "Modelo",
-                        "link" : "model.html",
-                        "rel"  : "Go to...",
-                        "id"   : "model"
-                    },
-                    {
-                        "name" : "Ventajas",
-                        "link" : "#",
-                        "rel"  : "Go to...",
-                        "id"   : "advantage"
-                    },
-                    {
-                        "name" : "Empleo",
-                        "link" : "#",
-                        "rel"  : "Go to...",
-                        "id"   : "job"
                     }
                 ]
             },
@@ -66,157 +24,31 @@ def get_navbar():
                 "name"  : "About me",
                 "id"    : "aboutMe",
                 "link"  : "tips.html",
-                "rel"   : "Go to...",
-                "submenu"   : [
-                    {
-                        "name" : "Consejos",
-                        "link" : "#",
-                        "rel"  : "Go to..."
-                    },
-                    {
-                        "name" : "Perfumeria",
-                        "link" : "#",
-                        "rel"  : "Go to...",
-                        "id"   : "perfumery"
-                    },
-                    {
-                        "name" : "Hogar",
-                        "link" : "#",
-                        "rel"  : "Go to...",
-                        "id"   : "home"
-                    },
-                    {
-                        "name" : "Alimentación",
-                        "link" : "#",
-                        "rel"  : "Go to...",
-                        "id"   : "feeding"
-                    },
-                    {
-                        "name" : "Mascotas",
-                        "link" : "pets.html",
-                        "rel"  : "Go to...",
-                        "id"   : "pets"
-                    }
-                ]
+                "rel"   : "Go to..."
             },
             {
                 "name"  : "Blog",
                 "id"    : "blog",
                 "link"  : "press.html",
-                "rel"   : "Go to...",
-                "submenu"   : [
-                    {
-                        "name" : "Prensa",
-                        "link" : "#",
-                        "rel"  : "Go to..."
-                    },
-                    {
-                        "name" : "Actualidad",
-                        "link" : "#",
-                        "rel"  : "Go to...",
-                        "id"   : "actiality"
-                    },
-                    {
-                        "name" : "Notas de prensa",
-                        "link" : "#",
-                        "rel"  : "Go to...",
-                        "id"   : "press"
-                    },
-                    {
-                        "name" : "Dosier",
-                        "link" : "#",
-                        "rel"  : "Go to...",
-                        "id"   : "dossier"
-                    },
-                    {
-                        "name" : "Galería multimedia",
-                        "link" : "#",
-                        "rel"  : "Go to...",
-                        "id"   : "gallery"
-                    },
-                    {
-                        "name" : "Estudios e informes",
-                        "link" : "#",
-                        "rel"  : "Go to...",
-                        "id"   : "reports"
-                    }
-                ]
+                "rel"   : "Go to..."
             },
             {
                 "name"  : "Calendar",
                 "id"    : "calendar",
                 "link"  : "support.html",
-                "rel"   : "Go to...",
-                "submenu"   : [
-                    {
-                        "name" : "Atención al cliente",
-                        "link" : "#",
-                        "rel"  : "Go to..."
-                    },
-                    {
-                        "name" : "Formulario de contacto",
-                        "link" : "#",
-                        "rel"  : "Go to...",
-                        "id"   : "formulary"
-                    },
-                    {
-                        "name" : "Preguntas frecuentes",
-                        "link" : "#",
-                        "rel"  : "Go to...",
-                        "id"   : "questions"
-                    }
-                ]
+                "rel"   : "Go to..."
             },
             {
                 "name"  : "FAQ",
                 "id"    : "faq",
                 "link"  : "support.html",
-                "rel"   : "Go to...",
-                "submenu"   : [
-                    {
-                        "name" : "Atención al cliente",
-                        "link" : "#",
-                        "rel"  : "Go to..."
-                    },
-                    {
-                        "name" : "Formulario de contacto",
-                        "link" : "#",
-                        "rel"  : "Go to...",
-                        "id"   : "formulary"
-                    },
-                    {
-                        "name" : "Preguntas frecuentes",
-                        "link" : "#",
-                        "rel"  : "Go to...",
-                        "id"   : "questions"
-                    }
-                ]
+                "rel"   : "Go to..."
             },
             {
                 "name"  : "Contact",
                 "id"    : "contact",
                 "link"  : "support.html",
-                "rel"   : "Go to...",
-                "submenu"   : [
-                    {
-                        "name" : "Atención al cliente",
-                        "link" : "#",
-                        "rel"  : "Go to..."
-                    },
-                    {
-                        "name" : "Formulario de contacto",
-                        "link" : "#",
-                        "rel"  : "Go to...",
-                        "id"   : "formulary"
-                    },
-                    {
-                        "name" : "Preguntas frecuentes",
-                        "link" : "#",
-                        "rel"  : "Go to...",
-                        "id"   : "questions"
-                    }
-                ]
+                "rel"   : "Go to..."
             }
         ]
-
     }
